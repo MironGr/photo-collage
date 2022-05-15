565c26ea867105f0aba286c0036568d9ba963871
Miron Kucher <Mirverch@yandex.ru>
2022-05-15 20:43:39 +0500

    feat: init commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,7 @@ import './App.scss';
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Template for SPA (create react app)</h1>
-      <ul>
-        <li><h3>React 18</h3></li>
-        <li><h3>Typescript</h3></li>
-        <li><h3>Redux (the store is created)</h3></li>
-        <li><h3>Redux Saga (is added to the store)</h3></li>
-        <li><h3>Sass</h3></li>
-        <li><h3>classnames (for dev)</h3></li>
-        <li><h3>yarn</h3></li>
-      </ul>
+      <h1>Photo Collage</h1>
     </div>
   );
 }
